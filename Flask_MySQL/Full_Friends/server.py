759f0aa78dac9eb7e5ac0b95facf1ad8895b66ea
Todd Enders <tenders64@gmail.com>
2017-11-02 13:11:36 -0700

    Full Friends Solved

---
+++ Flask_MySQL/Full_Friends/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+mysql = MySQLConnector(app, 'friends')
+
+@app.route('/')
+def index():
+    query = "SELECT * FROM friends"
+    friends = mysql.query_db(query)
+    return render_template('index.html', friends=friends)
+
+@app.route('/create', methods=["POST"])
+def create():
+    query = "INSERT INTO friends (name, age, created_at, updated_at) VALUES (:name, :age, NOW(), NOW())"
+    data = {
+        'name': request.form['name'],
+        'age': request.form['age']
+    }
+
+    mysql.query_db(query, data)
+    return redirect('/')
+app.run(debug=True)
